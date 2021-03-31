aeb44cdb021b63d2a31061cab2b5845cc34288c6
Dmitriy <dbryndin88@yandex.ru>
2021-04-01 00:54:00 +0300

    7

--- posts/views.py
+++ posts/views.py
@@ -69,7 +69,7 @@ def profile(request, username):
 def post_view(request, username, post_id):
     """"Представление страницы отдельной записи"""
     author = get_object_or_404(User, username=username)
-    post = get_object_or_404(Post, id=post_id, author__username=username)
+    post = get_object_or_404(Post, id=post_id)
     return render(request, "post.html", {"author": author, "post": post})
 
 
